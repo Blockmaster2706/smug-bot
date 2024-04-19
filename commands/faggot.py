6b36f8d4ed52dd35d03f3ed158dcd1f10b828c10
Blockmaster2706 <blockyork@live.de>
2024-04-19 18:48:36 +0200

    Add faggot command

--- commands/faggot.py
+++ commands/faggot.py
@@ -6,15 +6,15 @@ from    permcheck   import permcheck
 plugin = lightbulb.Plugin("Faggot")
 
 @plugin.command()
-@lightbulb.option("user", "The user to faggot", type=6, required=True, autocomplete=True)
+@lightbulb.option("user", "The user to faggot", type=hikari.OptionType.USER, required=True)
 @lightbulb.command("faggot", "Calls someone else a faggot.")
 @lightbulb.implements(lightbulb.SlashCommand)
-async def command(ctx: lightbulb.Context) -> None:
+async def faggot(ctx: lightbulb.Context) -> None:
 
     await cmd_log(ctx)
 
     if await permcheck(ctx):
-        await ctx.respond("Faggot!")
+        await ctx.respond("Faggot! " + ctx.options.user.mention, user_mentions=True)
         return
     else:
         ctx.respond("No Perms")
